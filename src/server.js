3327007db88e134afc210d5d3d0ced77ad9368c1
Paul Craig <paul_craig_16@hotmail.com>
2018-06-12 18:53:19 -0400

    add emotion support

--- src/server.js
+++ src/server.js
@@ -1,29 +1,33 @@
-import express from 'express';
-import { render } from '@jaredpalmer/after';
-import routes from './routes';
+import express from 'express'
+import { render } from '@jaredpalmer/after'
+import { renderToString } from 'react-dom/server'
+import routes from './routes'
+import { renderStylesToString } from 'emotion-server'
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
-const server = express();
+const server = express()
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', async (req, res) => {
+    const customRenderer = node => ({
+      html: renderStylesToString(renderToString(node)),
+    })
+
     try {
       const html = await render({
         req,
         res,
+        customRenderer,
         routes,
         assets,
-        // Anything else you add here will be made available
-        // within getInitialProps(ctx)
-        // e.g a redux store...
-        customThing: 'thing',
-      });
-      res.send(html);
+      })
+      res.send(html)
     } catch (error) {
-      res.json(error);
+      console.log(error)
+      res.json(error)
     }
-  });
+  })
 
-export default server;
+export default server
